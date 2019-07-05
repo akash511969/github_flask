d3cba765fdb71ccf2efd420638247fdf62aee94d
akash511969 <akash511969@gmail.com>
2019-07-05 08:25:57 +0530

    first commit

--- github_flask/git_flask.py
+++ github_flask/git_flask.py
@@ -1,6 +1,9 @@
 from flask import *
+import requests
 from github import Github
+from pprint import pprint
 app = Flask(__name__)
+
 
 @app.route('/')
 def login():
@@ -8,6 +11,31 @@ def login():
 
 
 
+# @app.route('/login')
+
+# def lo():
+# 	msg="msg"
+# 	try: 
+# 		nm=request.args.get("username")
+# 		password=request.args.get("pass")
+# 		g = Github(nm, password)
+
+		
+# 	except:
+# 		msg = "wrong password"  
+# 	finally:
+		# user = g.get_user()
+		# a=user.login
+		# base_url='https://api.github.com/users/'
+		# end_url='/repos'
+		# complete_url=base_url+a+end_url
+		# response=requests.get(complete_url)
+		# x=response.json()
+		# pprint(x)
+		# z=[]
+		# for i in x:
+		# 	z.append(i["name"])
+		# return render_template('success.html',name_html=nm,pass_html=password)
 @app.route('/login')
 
 def lo():
@@ -23,8 +51,20 @@ def lo():
 	finally:
 		user = g.get_user()
 		a=user.login
+		base_url='https://api.github.com/users/'
+		end_url='/repos'
+		complete_url=base_url+a+end_url
+		response=requests.get(complete_url)
+		x=response.json()
+		# Avatar url
+		v=x[0]['owner']["avatar_url"]
+		# Repo namelist
+		a=[]
+		for i in x:
+			a.append(i["name"])
 		
-		return render_template('success.html',name_html=nm,pass_html=password,a_html=a)
+		return render_template('success.html',len=len(a),a=a,name_html=nm,pass_html=password,a_html=a,avatar_html=v)
+
 
 if __name__ == '__main__':
 	# app.run()
